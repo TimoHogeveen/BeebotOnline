984e31c47cc90ac0bb72dfb9f69bb81cbb5128ae
TimoHogeveen <37137316+TimoHogeveen@users.noreply.github.com>
2021-04-15 13:31:13 +0200

    Afbeeldingen uploaden kan nu!
    https://symfony.com/doc/current/controller/upload_file.html

--- src/Controller/MatController.php
+++ src/Controller/MatController.php
@@ -9,6 +9,9 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Symfony\Component\String\Slugger\SluggerInterface;
 
 /**
  * @Route("/mat")
@@ -28,13 +31,47 @@ class MatController extends AbstractController
     /**
      * @Route("/new", name="mat_new", methods={"GET","POST"})
      */
-    public function new(Request $request): Response
+    public function new(Request $request, SluggerInterface $slugger): Response
     {
         $mat = new Mat();
         $form = $this->createForm(MatType::class, $mat);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
+
+
+
+
+        /** @var UploadedFile $imageFile */
+        $imageFile = $form->get('image')->getData();
+
+        // this condition is needed because the 'image' field is not required
+        // so the PDF file must be processed only when a file is uploaded
+        if ($imageFile) {
+            $originalFilename = pathinfo($imageFile->getClientOriginalName(), PATHINFO_FILENAME);
+            // this is needed to safely include the file name as part of the URL
+            $safeFilename = $slugger->slug($originalFilename);
+            $newFilename = $safeFilename.'-'.uniqid().'.'.$imageFile->guessExtension();
+
+            // Move the file to the directory where images are stored
+            try {
+                $imageFile->move(
+                    $this->getParameter('images_directory'),
+                    $newFilename
+                );
+            } catch (FileException $e) {
+                // ... handle exception if something happens during file upload
+            }
+
+            // updates the 'imageFilename' property to store the PDF file name
+            // instead of its contents
+            $mat->setimage($newFilename);
+
+
+
+
+
+        }
             $entityManager = $this->getDoctrine()->getManager();
             $entityManager->persist($mat);
             $entityManager->flush();
--- src/Form/MatType.php
+++ src/Form/MatType.php
@@ -6,6 +6,8 @@ use App\Entity\Mat;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Validator\Constraints\File;
 
 class MatType extends AbstractType
 {
@@ -13,7 +15,29 @@ class MatType extends AbstractType
     {
         $builder
             ->add('name')
-            ->add('image')
+            ->add('image', FileType::class, [
+                'label' => 'Mat (Afbeelding)',
+
+                // unmapped means that this field is not associated to any entity property
+                'mapped' => false,
+
+                // make it optional so you don't have to re-upload the PDF file
+                // every time you edit the Product details
+                'required' => false,
+
+                // unmapped fields can't define their validation using annotations
+                // in the associated entity, so you can use the PHP constraint classes
+                'constraints' => [
+                    new File([
+                        'maxSize' => '30M',
+                        'mimeTypes' => [
+                            'image/png',
+                            'image/jpeg',
+                        ],
+                        'mimeTypesMessage' => 'Upload een mat',
+                    ])
+                ],
+            ])
             ->add('category')
             ->add('user')
         ;
