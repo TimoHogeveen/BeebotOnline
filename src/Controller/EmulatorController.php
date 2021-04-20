bf58ab5ffef4cbfb5ceff22da9f045b0cade02e4
TimoHogeveen <37137316+TimoHogeveen@users.noreply.github.com>
2021-04-20 10:31:23 +0200

    Category dropdown

--- src/Controller/EmulatorController.php
+++ src/Controller/EmulatorController.php
@@ -5,16 +5,19 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Category;
+use App\Repository\CategoryRepository;
 
 class EmulatorController extends AbstractController
 {
     /**
      * @Route("/", name="emulator")
      */
-    public function index(): Response
+    public function index(CategoryRepository $categoryRepository): Response
     {
         return $this->render('emulator/index.html.twig', [
             'controller_name' => 'EmulatorController',
+            'categories' => $categoryRepository->findAll(), 
         ]);
     }
 }
