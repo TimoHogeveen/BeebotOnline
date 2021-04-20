439a13c4d60c1452e5a190a410a983d5dd1b470f
TimoHogeveen <37137316+TimoHogeveen@users.noreply.github.com>
2021-04-20 10:37:50 +0200

    Dropdown voor matten
    Nog niet gebaseerd op de categorie

--- src/Controller/EmulatorController.php
+++ src/Controller/EmulatorController.php
@@ -5,19 +5,20 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use App\Entity\Category;
 use App\Repository\CategoryRepository;
+use App\Repository\MatRepository;
 
 class EmulatorController extends AbstractController
 {
     /**
      * @Route("/", name="emulator")
      */
-    public function index(CategoryRepository $categoryRepository): Response
+    public function index(CategoryRepository $categoryRepository, MatRepository $matRepository): Response
     {
         return $this->render('emulator/index.html.twig', [
             'controller_name' => 'EmulatorController',
-            'categories' => $categoryRepository->findAll(), 
+            'categories' => $categoryRepository->findAll(),
+            'mats' => $matRepository->findAll(), 
         ]);
     }
 }
