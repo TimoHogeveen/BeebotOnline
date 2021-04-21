bb98aeb098b1ca4ce0bb6b2ac817ce2306195c94
TimoHogeveen <37137316+TimoHogeveen@users.noreply.github.com>
2021-04-21 13:09:10 +0200

    Tweede dropdown gebaseerd op categorie

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,29 @@
 $('.categorydropdown').on('input', function(){ 
-    console.log($(this).val());
-    var dropdownval = $(this).val();
+    var id = $(this).val();
+
+$.ajax({
+
+url:'mats/'+id,
+method: 'GET',
+dataType: 'html',
+
+
+}).done(function(response){
+    console.log(response);
+    //$('.mattendropdown').empty(); // <<<<<< No more issue here
+
+    //$.each(response.matten, function(id) {
+        //$('.mattendropdown').append( new Option(response.matten[id]) );
+        $('.mattendropdown').html(response);
+    //});
+
 })
+
+
+
+})
+
+
+
+
+
--- src/Controller/EmulatorController.php
+++ src/Controller/EmulatorController.php
@@ -4,9 +4,11 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Repository\CategoryRepository;
 use App\Repository\MatRepository;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 class EmulatorController extends AbstractController
 {
@@ -21,4 +23,24 @@ class EmulatorController extends AbstractController
             'mats' => $matRepository->findAll(), 
         ]);
     }
+    /**
+     * @Route("/mats/{id}", name="matsid", methods={"GET"})
+     */
+    public function matsid(Request $request, MatRepository $matRepository): Response
+    {
+        $id = $request->get('id');
+
+        //if($request->isXmlHttpRequest()){
+            $matten = $matRepository->findBy(array('category' => $id));
+  
+        // }
+        $s = "";
+         foreach ($matten as $mat) {
+             $s .= "<option>". $mat->getName().' </option> ';
+}
+        return $response = new Response($s);
+
+    }
+
+    
 }
