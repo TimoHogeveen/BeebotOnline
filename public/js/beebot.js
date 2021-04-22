482fd9559a0719031c18e5c2c53cc87ebb47a66c
TimoHogeveen <37137316+TimoHogeveen@users.noreply.github.com>
2021-04-22 12:34:57 +0200

    Voorbereiding Javascript, nog niet af. Ook CSS beter gemaakt

---
+++ public/js/beebot.js
@@ -0,0 +1,423 @@
+var audio_click, audio_stop, audio_motor, audio_full;
+var bot, select, link, grid, angle, margin;
+
+var botimg, dragimg = new Image();
+
+var mats;
+var currentMat = null;
+var currentIndex = -1;
+
+function setup() {
+    botimg = document.getElementById("botimg");
+    dragDataType = 'text/plain';
+    timerID = 0;
+
+    audio_click = new Audio();
+    audio_click.src = "/assets/audio/beebot-button.mp3";
+    audio_stop = new Audio();
+    audio_stop.src = "/assets/audio/beebot-stop.mp3";
+    audio_motor = new Audio();
+    audio_motor.src = "/assets/audio/beebot-motor.mp3";
+    audio_full = new Audio();
+    audio_full.src = "/assets/audio/beebot-full.mp3";
+
+    bot = document.getElementById("bot");
+    select = document.getElementById("mats");
+    link = document.getElementById("link");
+    grid = null;  // takes the current grid image, see select_mat()
+
+    var style = getComputedStyle(document.getElementById("playground"));
+    margin = parseInt(style.paddingTop);
+
+    enable_buttons(true);
+    if (!mats)
+        loadMatsJson();
+    else
+        load_mats();
+}
+
+function loadMatsJson() {
+    loadFile("mats/mats_list.json", function(text) {
+        if (text) {
+            mats = JSON.parse(text);
+            load_mats();
+        }
+        else
+            alert ("Cannot load the mats list!");
+    });
+}
+
+function loadInfo() {
+    if (!currentMat || location.protocol === "file:")
+        return;
+    var div = document.getElementById("right-info");
+    div.innerHTML = "";
+    if (currentMat.html)
+        div.innerHTML = currentMat.html;
+    else {
+        function getBody(text) {
+            let s = /<body.*?>([\s\S]*)<\/body>/m.exec(text);
+            return s ? s[1] : "";
+        }
+        var filename = currentMat.image.split(".");
+        filename[filename.length-1] = "html";
+        loadFile("mats/" + filename.join("."), function(text) {
+            if (text)
+                div.innerHTML = currentMat.html = getBody(text);
+            else
+                loadFile("info.html", function(text) {
+                    if (text)
+                        div.innerHTML = currentMat.html = getBody(text);
+                }
+            );
+        });   
+    }
+}
+
+function loadFile(url, cb) {
+    var xmlhttp = new XMLHttpRequest();
+ 
+    xmlhttp.onreadystatechange = function() {
+        if (this.readyState != 4)
+            return;
+        if (this.status == 200)
+            cb(this.responseText);
+        else
+            cb();
+    };
+    xmlhttp.open("GET", url, true);
+    xmlhttp.send();
+ }
+
+// the variable "mats" has been loaded from mats/mats_list.json
+// mats = {};
+
+// starting cell if given via query parameter
+var start = { x: 0, y: 0, angle: -1 };
+
+function load_mats() {
+	var first = -1;
+    var mat = "", single = false;
+    let search = new URLSearchParams(location.search);
+    if (search.has("start")) {
+        let arr = search.get("start").split(",");
+        if (arr.length > 0) {
+            start.x = +arr[0].trim() || 0;
+            if (start.x < 1)
+                start.x = 0;
+        }
+        if (arr.length > 1) {
+            start.y = +arr[1].trim() || 0;
+            if (start.y < 1)
+                start.y = 0;
+        }
+        if (arr.length > 2) {
+            start.angle = arr[2].trim() || -1;
+            if (start.angle >= 0)
+                start.angle = Math.round(start.angle / 90) * 90;
+        }
+    }
+
+    for(let pair of search.entries()) {
+        let [key, value] = pair;
+        if (!value) {
+            // assume a mat
+            mat = decodeURIComponent(key);
+            single = mat.endsWith("1");
+            if (single)
+                mat = mat.slice(0, -1);
+            first = mats.findIndex(function(obj) { return obj.sku === mat });
+            if (first < 0) {
+                document.getElementById("pagename").innerText = mat;
+                document.getElementById("container").style.display = "none";
+                document.getElementById("help").style.display = "none";
+                document.getElementById("notfound").style.display = "block";
+                return;
+            }
+            if (single) {
+                mats = [ mats[first] ];
+                first = -1;
+                select = null;
+            }
+            else
+                start = { x: 0, y: 0, angle: -1 };     // start coords only valid in single mat mode
+            break;
+        }
+    }
+    var playground = document.getElementById("playground");
+    var index = 0;
+    if (mats.length === 1)
+        document.getElementById("mat_selector").innerHTML = mats[0].name;
+
+    for (var i = 0; i < mats.length; i++) {
+        var obj = mats[i];
+        obj.index = i;
+        // Find first mat w/o password
+        if (first < 0 && !obj.password)
+            first = i;
+        var html = document.createElement("img");
+        html.classList.add("box");
+        html.id = "mat" + index;
+        html.src = "mats/" + obj.image;
+        html.style.display = "none";
+        obj.img = html;
+        playground.appendChild(html);
+        if (select) {
+            html = document.createElement("option");
+            html.value = obj.sku;
+            html.innerHTML = obj.name;
+            select.appendChild(html);
+            index++;
+        }
+    }
+    if (first < 0)
+        first = 0;
+    if (select)
+        select.value = first;
+    setTimeout(function () {
+        // wait a split second so rendering is finished
+        if (select)
+            select.selectedIndex = first;
+        select_mat(first);
+        setDropHandlers(mats);
+        var style = getComputedStyle(document.getElementById("mat0"));
+        margin += parseInt(style.paddingTop || "0") + parseInt(style.borderTop || "0");
+    }, 200);
+}
+
+function set_angle(deg) {
+    angle = deg;
+    var c = document.getElementById("bot");
+    var w = Math.max(botimg.width, botimg.height);
+    c.width = c.height = w;
+    var ctx = c.getContext("2d");
+    ctx.clearRect(0, 0, w, w);
+    ctx.translate(w/2, w/2);
+    ctx.rotate(angle * Math.PI / 180);
+    ctx.translate(-w/2, -w/2);
+    ctx.drawImage(botimg, 0, 0);
+    dragimg.src = c.toDataURL();
+}
+
+function addCmd(what) {
+    var span = document.getElementById("commands");
+    if (span.getElementsByTagName("img").length >= 40) {
+        audio_full.play();
+        return;
+    }
+    var img = document.createElement("img");
+    img.src = "/assets/img/go-" + what + ".png";
+    span.appendChild(img);
+}
+
+function clear() {
+    var span = document.getElementById("commands");
+    span.innerHTML = "";
+}
+
+var curIndex = -1;
+var curElement = null;
+var elements = null;
+
+function run() {
+    curIndex = 0;
+    var span = document.getElementById("commands");
+    elements = span.getElementsByTagName("img");
+    enable_buttons(false);
+    nextStep();
+}
+
+// Play the next element on the list of steps
+function nextStep() {
+    setCurrentElement(curIndex++);
+    if (!curElement)
+        stop();
+    else {
+        var file = curElement.attributes.src.value.split("/").pop();
+          switch (file) {
+            case 'go-pause.png': pause(); break;
+            case 'go-fd.png': move(true); break;
+            case 'go-bk.png': move(false); break;
+            case 'go-lt.png': rotate(false); break;
+            case 'go-rt.png': rotate(true); break;
+        }
+    }
+}
+
+function setCurrentElement(index) {
+    if (curElement)
+        curElement.style.border = "none";
+    curElement = (index >= 0 && elements && index < elements.length) ? elements[index] : null;
+    if (curElement)
+        curElement.style.border = "2px solid black";
+}
+// Sprite movement: use left and top margins to place the sprite
+function move(fd) {
+    var endVal = currentMat.units;
+    var oldAngle = angle;
+    if (!fd) {
+        switch (angle) {
+            case 0: angle = 180; break;
+            case 90: angle = 270; break;
+            case 180: angle = 0; break;
+            case 270: angle = 90; break;
+        }
+    }
+    audio_motor.load();
+    timerID = setInterval(function () {
+        var x = bot.offsetLeft;
+        var y = bot.offsetTop;
+        var diff = 5;
+        switch (angle) {
+            case 0: y -= diff; break;
+            case 90: x += diff; break;
+            case 180: y += diff; break;
+            case 270: x -= diff; break;
+        }
+        if (x < 0) x = 0;
+        if (y < 0) y = 0;
+        if (x > (grid.clientWidth + 3 * margin / 2))
+            x = grid.clientWidth + 3 * margin / 2;
+        if (y > (grid.clientHeight + 3 * margin / 2))
+            y = grid.clientHeight + 3 * margin / 2;
+        bot.style.marginLeft = x + "px";
+        bot.style.marginTop = y + "px";
+        endVal -= diff;
+        if (endVal <= 0) {
+            clearInterval(timerID);
+            timerID = 0;
+            angle = oldAngle;
+            audio_motor.pause();
+            audio_click.play();
+            nextStep();
+        }
+    }, 100);
+    audio_motor.play();
+}
+
+function rotate(right) {
+    var endVal = angle + (right ? 90 : -90);
+    if (endVal < 0)
+        endVal += 360;
+    if (endVal >= 360)
+        endVal -= 360;
+    audio_motor.load();
+    timerID = setInterval(function () {
+        var deg = angle;
+        if (right)
+            deg += 10;
+        else
+            deg -= 10;
+        if (deg < 0)
+            deg += 360;
+        if (deg >= 360)
+            deg -= 360;
+        if (Math.abs(endVal - deg) < 1)
+            deg = endVal;
+        set_angle(deg);
+        if (deg == endVal) {
+            clearInterval(timerID);
+            timerID = 0;
+            audio_motor.pause();
+            audio_click.play();
+            nextStep();
+        }
+    }, 100);
+    audio_motor.play();
+}
+
+function pause() {
+    timerID = setTimeout(function () {
+        clearTimeout(timerID);
+        timerID = 0;
+        audio_click.play();
+        nextStep();
+    }, 2000);
+}
+
+function stop() {
+    if (timerID)
+        clearInterval(timerID);
+    timerID = 0;
+    audio_motor.pause();
+    audio_stop.play();
+    enable_buttons(true);
+    setCurrentElement(-1);
+}
+
+function enable_buttons(enabled) {
+    for (var i in buttons) {
+        var b = document.getElementById(buttons[i]);
+        b.disabled = !enabled;
+        b = document.getElementById("stop_run");
+        b.disabled = enabled;
+    }
+}
+
+function select_mat(index) {
+    currentMat = mats[index];
+    if (grid)
+        grid.style.display = "none";
+    if (currentMat.password) {
+        var pass = prompt("Please enter the secret password for this mat:");
+        if (pass !== currentMat.password) {
+            alert("Sorry, you are not allowed to access this mat!");
+            if (select)
+                select.selectedIndex = currentIndex;
+            bot.style.display = "none";
+            if (currentIndex < 0)
+                return;
+        }
+    }
+    // need only to enter the password once
+    currentMat.password = "";
+    grid = document.getElementById("mat" + index);
+    grid.style.display = "block";
+    // Show or hide the link to the product page
+    if (currentMat.url) {
+        document.getElementById("link-container").style.display = "block";
+        link.setAttribute('href', "https://www.terrapinlogo.com" + currentMat.url);
+    }
+    else
+        document.getElementById("link-container").style.display = "none";
+
+    currentIndex = index;
+    clear();
+    loadInfo();
+    setTimeout(home, 100);
+    var c = document.getElementById("container");
+    if (typeof chrome != "undefined") {
+        if ("window" in chrome.app) {
+            var curWindow = chrome.app.window.current();
+            curWindow.innerBounds.width = c.clientWidth + 2 + c.offsetLeft;
+            curWindow.innerBounds.height = c.clientHeight + 2 * c.offsetTop;
+        }
+    }
+}
+
+function home() {
+    bot.style.display = "block";
+    var w = bot.clientWidth + currentMat.units;
+    var h = bot.clientHeight + currentMat.units;
+    var cx = Math.round(currentMat.img.width / currentMat.units);
+    var cy = Math.round(currentMat.img.height / currentMat.units);
+    var x = start.x;
+    var y = start.y;
+    var angle = start.angle;
+    if (x && y) {
+        if (x > cx) x = cx;
+        if (y > cy) y = cy;
+        x *= currentMat.units;
+        y *= currentMat.units;
+    }
+    else {
+        x = currentMat.start[0];
+        y = currentMat.start[1];
+    }
+    if (angle < 0)
+        angle = currentMat.angle || 0;
+    bot.style.marginLeft = (x - w / 2 + margin) + "px";
+    bot.style.marginTop = (y - h / 2 + margin) + "px";
+
+    set_angle(angle);
+}
---
+++ public/js/handlers.js
@@ -0,0 +1,99 @@
+buttons = ["fd", "bk", "lt", "rt", "go", "stop", "pause", "home"];
+// contains the JSON from mats/mats_list.js
+// mats = {};
+// the current mat
+// currentMat = null;
+
+timerID = 0;
+
+document.addEventListener('DOMContentLoaded', function() 
+{
+	// do the global setup
+	setup();
+	// button_click() handles the button clicks
+	for (var i in buttons)
+		document.getElementById(buttons[i]).addEventListener("click", function(evt)
+	{
+		audio_click.play();
+		switch (this.id) {
+			case 'pause':
+			case 'fd':
+			case 'bk':
+			case 'lt':
+			case 'rt':	addCmd(this.id); break;
+			case 'go':	run(); break;
+			case 'stop': clear(); break;
+		}
+	});
+	
+	// When the bot is clicked, its angle resets
+/*	document.getElementById("bot").addEventListener("click", function()
+	{
+		set_angle(currentMat ? currentMat.angle : 0);
+	});
+*/	
+	// The selection handler for the mats DDL (if present)
+	(function() {
+		if (select)
+			select.addEventListener("change", function()
+			{		
+				select_mat(this.selectedIndex);
+			});
+	})();
+
+	// The Home handler
+	document.getElementById("home").addEventListener("click", function()
+	{
+		home();
+	});
+	
+	// The Stop Run handler
+	document.getElementById("stop_run").addEventListener("click", function()
+	{
+		stop();
+	});
+	
+});
+
+function setDropHandlers(mats) {
+	// drag and drop handlers
+	document.getElementById("bot").addEventListener("dragstart", function(evt)
+	{
+		var offset = { x: evt.offsetX, y: evt.offsetY };
+		var dt = evt.dataTransfer;
+		if (dt.setDragImage)
+			dt.setDragImage(dragimg, offset.x, offset.y);
+		// click offset is the drag data
+		dt.setData("text", JSON.stringify(offset));
+		dt.effectAllowed = "move";
+		dt.dropEffect = "move";
+		var bot = this;
+		setTimeout(function() {
+			bot.style.visibility = "hidden";
+		}, 1);
+		return false;
+	});
+	document.getElementById("bot").addEventListener("dragend", function(evt)
+	{
+		evt.preventDefault();
+		setTimeout(function() {
+			bot.style.visibility = "";
+		}, 1);
+	});
+
+	for (var i in mats) {
+		var mat = document.getElementById("mat"+i);
+		mat.addEventListener("dragover", function(evt)
+		{
+			evt.preventDefault();
+		});
+		
+		mat.addEventListener("drop", function(evt)
+		{
+			evt.preventDefault();
+			var pos = JSON.parse(evt.dataTransfer.getData("text"));
+			bot.style.marginLeft = (evt.layerX - pos.x) + "px";
+			bot.style.marginTop = (evt.layerY - pos.y) + "px";
+		});
+	}
+}
