41dd31af26334406559d8a14ef77fa3606c289bc
TimoHogeveen <37137316+TimoHogeveen@users.noreply.github.com>
2021-05-10 10:37:02 +0200

    Nieuwe login redirect

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -15,7 +15,7 @@ class SecurityController extends AbstractController
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
         if ($this->getUser()) {
-            return $this->redirectToRoute('emulator');
+            return $this->redirectToRoute('mat_index');
         }
 
         // get the login error if there is one
